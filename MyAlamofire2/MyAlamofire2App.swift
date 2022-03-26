2602967226049c85d358eb2beed92e4d79fe4743
박힘찬 <himchan05@naver.com>
2022-03-26 16:10:51 +0900

    Initial Commit

---
+++ MyAlamofire2/MyAlamofire2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyAlamofire2App.swift
+//  MyAlamofire2
+//
+//  Created by 박힘찬 on 2022/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MyAlamofire2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
